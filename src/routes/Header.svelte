d45052d5ae984441bea54874af65de74b89f29ae
Wordllban <mininkov3228@gmail.com>
2024-11-30 02:23:59 +0200

    shadcn setup, style background with grid pattern

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,5 +1,6 @@
-<nav>
-	<a href="/stack">Stack</a>
+<nav class="flex w-full items-center justify-center gap-24 p-8 text-2xl">
+	<a href="/skills">Skills</a>
 	<a href="/projects">Projects</a>
 	<a href="/resume">Resume</a>
+	<div>Socials list</div>
 </nav>
